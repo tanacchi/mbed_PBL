19765ce9275a4fa204e85e78e7740b180d68659f
tanacchi <q111026d@mail.kyutech.jp>
2016-11-24 16:17:53 +0900

    Add comment out in Main
    Main/ 内の関数にはコメントで説明

--- Main/exchange_NUMtoARY.cpp
+++ Main/exchange_NUMtoARY.cpp
@@ -1,20 +1,25 @@
-int* exchange_NUMtoARY(int num){
+#define ON 1
+#define OFF 0
+
+int* exchange_NUMtoARY(int num){ // 引数のint型整数に対応するON/OFFの並び(配列)を渡してくれる
 
   int* dest;
   const int sevseg_ary[10][7] = {
-    {ON,  ON,  ON,  ON,  ON,  ON , OFF},
-    {OFF, ON,  ON,  OFF, OFF, OFF, OFF},
-    {ON,  ON,  OFF, ON,  ON,  OFF, ON },
-    {ON,  ON,  ON,  ON,  OFF, OFF, ON },
-    {OFF, ON , ON,  OFF, OFF, ON,  ON },
-    {ON,  OFF, ON,  ON,  ON,  OFF, ON },
-    {ON,  OFF, ON,  ON,  ON,  ON,  ON },
-    {ON,  ON,  ON,  OFF, OFF, OFF, OFF},
-    {ON,  ON,  ON,  ON,  ON,  ON,  ON },
-    {ON,  ON,  ON,  ON,  OFF, ON,  ON }
+    //a    b    c    d    e    f    g
+    {ON,  ON,  ON,  ON,  ON,  ON , OFF}, // for 0
+    {OFF, ON,  ON,  OFF, OFF, OFF, OFF}, // for 1
+    {ON,  ON,  OFF, ON,  ON,  OFF, ON }, // for 2
+    {ON,  ON,  ON,  ON,  OFF, OFF, ON }, // for 3
+    {OFF, ON , ON,  OFF, OFF, ON,  ON }, // for 4
+    {ON,  OFF, ON,  ON,  ON,  OFF, ON }, // for 5
+    {ON,  OFF, ON,  ON,  ON,  ON,  ON }, // for 6
+    {ON,  ON,  ON,  OFF, OFF, OFF, OFF}, // for 7
+    {ON,  ON,  ON,  ON,  ON,  ON,  ON }, // for 8
+    {ON,  ON,  ON,  ON,  OFF, ON,  ON }  // for 9
   };
+  // ON = 1  // OFF = 0 
   
   return dest = sevseg_ary[num];
 
-  // exchange_NUMtoARY(numx)[i]
+  // exchange_NUMtoARY(num)[i]
 }

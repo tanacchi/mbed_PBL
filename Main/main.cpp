4fd43ca3a7bc0f4d2592a0d85499c4787ce558c3
tanacchi <q111026d@mail.kyutech.jp>
2016-12-04 02:20:17 +0900

    Correct indent main.cpp

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,15 +1,14 @@
 // #include "mbed.h"
-#include <stdio.h>
 
 #define ON 1
 #define OFF 0
 
 int* exchange_NUMtoARY(int num);
 double powpow(int a, int b);
-int* split_Numerical_Pos(double dd);
-void array_maker(double dd, int* toInput);
+int* split_Numerical_Pos(double tmp);
+void array_maker(double tmp, int* toInput);
 void input_inteder_ary(int* fromSplit, int toOutput[3][7]);
- 
+
 int sevseg_ary[10][7] = {
   {ON,  ON,  ON,  ON,  ON,  ON , OFF}, // for 0
   {OFF, ON,  ON,  OFF, OFF, OFF, OFF}, // for 1
@@ -22,27 +21,20 @@ int sevseg_ary[10][7] = {
   {ON,  ON,  ON,  ON,  ON,  ON,  ON }, // for 8
   {ON,  ON,  ON,  ON,  OFF, ON,  ON }  // for 9
 };
-  
-int main(void){
 
-  int splited_num[3];
-  int i, j;
-  double dd = 79.1;
-  int segment_array[3][7] = {};
+int main(void){
   
-  array_maker(dd, splited_num);
+  double tmp = 23.5;
+  int splited_num[3];
+  int segment_array[3][7];
+  
+  array_maker(tmp, splited_num);
   input_inteder_ary(splited_num, segment_array);
 
-  for (i = 0; i < 3; i++){
-    for (j = 0; j < 7; j++)
-      printf("%d", segment_array[i][j]);
-    putchar('\n');
-  }
 }
 
 int* exchange_NUMtoARY(int num){
-  int* dest;
-  return dest = sevseg_ary[num];
+  return sevseg_ary[num];
 }
 
 double powpow(int a, int b){
@@ -53,27 +45,27 @@ double powpow(int a, int b){
     for (i = 0; i < b; i++) dest *= (double)a;
   if (b < 0)
     for (i = 0; i > b; i--) dest /= (double)a;
-  
+
   return dest;
 }
 
-int* split_Numerical_Pos(double dd){ 
-  int ary[3] = {};
-  int *dest_num = ary;
+int* split_Numerical_Pos(double tmp){
+  int ary[3];
+  int* dest_num = ary;
   int  i, j;
-
-  dd += 0.05; 
+  
+  tmp += 0.05; 
   
   for (i = 1; i > -2; i--){ 
-    for (j = 0; dd >= powpow(10, i); j++) dd -=powpow(10, i);
+    for (j = 0; tmp >= powpow(10, i); j++) tmp -=powpow(10, i);
     ary[1-i] = j;
   }
   return dest_num;
 }
 
-void array_maker(double dd, int* toInput){
+void array_maker(double tmp, int* toInput){
   int i;
-  for (i = 0; i < 3; i++) toInput[i] = split_Numerical_Pos(dd)[i];
+  for (i = 0; i < 3; i++) toInput[i] = split_Numerical_Pos(tmp)[i];
 }
 
 void input_inteder_ary(int* splited_num, int segment_array[3][7]){

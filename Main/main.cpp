f51024de858ecc8133fad8d65e462236e7f252a2
tanacchi <q111026d@mail.kyutech.jp>
2016-12-04 23:00:45 +0900

    Use #define and int i; in for()
    今できることはこれくらいしかなさそう　クラスのお勉強が急務だなあ

--- Main/main.cpp
+++ Main/main.cpp
@@ -2,14 +2,18 @@
 
 #define ON 1
 #define OFF 0
+#define SEGMENT_NUM 7
+#define NUM_PATTERN 10
+
+#define TMP_WIDTH 3
 
 int* exchange_NUMtoARY(int num);
 double powpow(int a, int b);
 int* split_Numerical_Pos(double tmp);
 void array_maker(double tmp, int* toInput);
-void input_inteder_ary(int* fromSplit, int toOutput[3][7]);
+void input_inteder_ary(int* fromSplit, int toOutput[TMP_WIDTH][SEGMENT_NUM]);
 
-int sevseg_ary[10][7] = {
+int sevseg_ary[NUM_PATTERN][SEGMENT_NUM] = {
   {ON,  ON,  ON,  ON,  ON,  ON , OFF}, // for 0
   {OFF, ON,  ON,  OFF, OFF, OFF, OFF}, // for 1
   {ON,  ON,  OFF, ON,  ON,  OFF, ON }, // for 2
@@ -23,14 +27,13 @@ int sevseg_ary[10][7] = {
 };
 
 int main(void){
-  
+
   double tmp = 23.5;
-  int splited_num[3];
-  int segment_array[3][7];
+  int splited_num[TMP_WIDTH];
+  int segment_array[TMP_WIDTH][SEGMENT_NUM];
   
   array_maker(tmp, splited_num);
   input_inteder_ary(splited_num, segment_array);
-
 }
 
 int* exchange_NUMtoARY(int num){
@@ -39,38 +42,35 @@ int* exchange_NUMtoARY(int num){
 
 double powpow(int a, int b){
   double dest = 1;
-  int i;
-  
+
   if (b > 0)
-    for (i = 0; i < b; i++) dest *= (double)a;
+    for (int i = 0; i < b; i++) dest *= (double)a;
   if (b < 0)
-    for (i = 0; i > b; i--) dest /= (double)a;
+    for (int i = 0; i > b; i--) dest /= (double)a;
 
   return dest;
 }
 
 int* split_Numerical_Pos(double tmp){
-  int ary[3];
-  int* dest_num = ary;
-  int  i, j;
-  
+  int ary[TMP_WIDTH];
+  int* dest = ary;
+  int i, j;
+
   tmp += 0.05; 
   
   for (i = 1; i > -2; i--){ 
     for (j = 0; tmp >= powpow(10, i); j++) tmp -=powpow(10, i);
     ary[1-i] = j;
   }
-  return dest_num;
+  return dest;
 }
 
 void array_maker(double tmp, int* toInput){
-  int i;
-  for (i = 0; i < 3; i++) toInput[i] = split_Numerical_Pos(tmp)[i];
+  for (int i = 0; i < TMP_WIDTH; i++) toInput[i] = split_Numerical_Pos(tmp)[i];
 }
 
-void input_inteder_ary(int* splited_num, int segment_array[3][7]){
-  int i, j;
-  for (i = 0; i < 3; i++)
-    for (j = 0; j < 7; j++)
+void input_inteder_ary(int* splited_num, int segment_array[TMP_WIDTH][SEGMENT_NUM]){
+  for (int i = 0; i < TMP_WIDTH; i++)
+    for (int j = 0; j < SEGMENT_NUM; j++)
       segment_array[i][j] = exchange_NUMtoARY(splited_num[i])[j];
 }

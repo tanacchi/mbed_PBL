ee39dbf35fcfa38e3edc82e8b2488ba1196c386f
tanacchi <q111026d@mail.kyutech.jp>
2016-12-06 01:09:07 +0900

    Test function added argment
    値の幅変えても引数で調節できるようになりました

--- Main/main.cpp
+++ Main/main.cpp
@@ -10,7 +10,7 @@
 int* exchange_NUMtoARY(int num);
 double powpow(int a, int b);
 int* split_Numerical_Pos(double tmp, int beginning, int end);
-void array_maker(double tmp, int* toInput, int digits);
+void array_maker(double tmp, int* toInput, int beginning, int end);
 void input_inteder_ary(int* fromSplit, int toOutput[DIGITS_NUM][SEGMENT_NUM], int digits);
 
 int sevseg_ary[NUM_PATTERN][SEGMENT_NUM] = {
@@ -26,20 +26,39 @@ int sevseg_ary[NUM_PATTERN][SEGMENT_NUM] = {
   {ON,  ON,  ON,  ON,  OFF, ON,  ON }  // for 9
 };
 
+void print_array(int src[3][7]){
+  for(int i = 0; i < 3; i++){
+    for (int j = 0; j < 7; j++) printf("%d ", src[i][j]);
+    putchar('\n');
+  }
+}
+
+void put_line(void){
+  for (int i = 0; i < 20; i++) putchar('-');
+  putchar('\n');
+}
+
 int main(void){
   
   double tmp = 23.5;
-  int splited_num[DIGITS_NUM];
-  int segment_array[DIGITS_NUM][SEGMENT_NUM];
-  
-  array_maker(tmp, splited_num, DIGITS_NUM);
-  input_inteder_ary(splited_num, segment_array, DIGITS_NUM);
+  int splited_tmp[DIGITS_NUM];
+  int temperature_ary[DIGITS_NUM][SEGMENT_NUM];
 
-  for (int i = 0; i < 3; i++){
-    for (int j = 0; j < 7; j++) printf("%d ", segment_array[i][j]);
-    putchar('\n');
-  }
+  int count = 340;
+  int splited_cnt[DIGITS_NUM];
+  int count_ary[DIGITS_NUM][SEGMENT_NUM];
+
+  array_maker(tmp, splited_tmp, 1, -1);
+  input_inteder_ary(splited_tmp, temperature_ary, DIGITS_NUM);
   
+  array_maker(count, splited_cnt, 2, 0);
+  input_inteder_ary(splited_cnt, count_ary, DIGITS_NUM);
+
+  put_line();
+  print_array(temperature_ary);
+  put_line();
+  print_array(count_ary);
+  put_line();
 }
 
 int* exchange_NUMtoARY(int num){
@@ -58,7 +77,7 @@ double powpow(int a, int b){
 }
 
 int* split_Numerical_Pos(double tmp, int beginning, int end){
-  const int width = beginning -end;
+  const int width = beginning - end + 1;
   int ary[width];
   int* dest_num = ary;
   int  i, j, k = 0;
@@ -72,8 +91,9 @@ int* split_Numerical_Pos(double tmp, int beginning, int end){
   return dest_num;
 }
 
-void array_maker(double tmp, int* toInput, int digits){
-  for (int i = 0; i < digits; i++) toInput[i] = split_Numerical_Pos(tmp, 1, -1)[i];
+void array_maker(double tmp, int* toInput, int beginning, int end){
+  const int width = beginning - end;
+  for (int i = 0; i < width; i++) toInput[i] = split_Numerical_Pos(tmp, beginning, end)[i];
 }
 
 void input_inteder_ary(int* splited_num, int segment_array[DIGITS_NUM][SEGMENT_NUM], int digits){

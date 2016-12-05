317ef0913ba3fbf87de8b52c85671ffb0b9da9da
tanacchi <q111026d@mail.kyutech.jp>
2016-12-06 01:40:29 +0900

    Checkout main.cpp before merge

--- Main/main.cpp
+++ Main/main.cpp
@@ -1,9 +1,7 @@
 // #include "mbed.h"
-#include <stdio.h>
 
 #define ON 1
 #define OFF 0
-<<<<<<< HEAD
 #define SEGMENT_NUM 7
 #define NUM_PATTERN 10
 
@@ -14,17 +12,6 @@ double powpow(int a, int b);
 int* split_Numerical_Pos(double tmp);
 void array_maker(double tmp, int* toInput);
 void input_inteder_ary(int* fromSplit, int toOutput[TMP_WIDTH][SEGMENT_NUM]);
-=======
-#define DIGITS_NUM 3
-#define NUM_PATTERN 10
-#define SEGMENT_NUM 7
-
-int* exchange_NUMtoARY(int num);
-double powpow(int a, int b);
-int* split_Numerical_Pos(double tmp, int beginning, int end);
-void array_maker(double tmp, int* toInput, int beginning, int end);
-void input_inteder_ary(int* fromSplit, int toOutput[DIGITS_NUM][SEGMENT_NUM], int digits);
->>>>>>> tana/add_arg
 
 int sevseg_ary[NUM_PATTERN][SEGMENT_NUM] = {
   {ON,  ON,  ON,  ON,  ON,  ON , OFF}, // for 0
@@ -39,47 +26,14 @@ int sevseg_ary[NUM_PATTERN][SEGMENT_NUM] = {
   {ON,  ON,  ON,  ON,  OFF, ON,  ON }  // for 9
 };
 
-void print_array(int src[3][7]){
-  for(int i = 0; i < 3; i++){
-    for (int j = 0; j < 7; j++) printf("%d ", src[i][j]);
-    putchar('\n');
-  }
-}
-
-void put_line(void){
-  for (int i = 0; i < 20; i++) putchar('-');
-  putchar('\n');
-}
-
 int main(void){
 
   double tmp = 23.5;
-<<<<<<< HEAD
   int splited_num[TMP_WIDTH];
   int segment_array[TMP_WIDTH][SEGMENT_NUM];
   
   array_maker(tmp, splited_num);
   input_inteder_ary(splited_num, segment_array);
-=======
-  int splited_tmp[DIGITS_NUM];
-  int temperature_ary[DIGITS_NUM][SEGMENT_NUM];
-
-  int count = 340;
-  int splited_cnt[DIGITS_NUM];
-  int count_ary[DIGITS_NUM][SEGMENT_NUM];
-
-  array_maker(tmp, splited_tmp, 1, -1);
-  input_inteder_ary(splited_tmp, temperature_ary, DIGITS_NUM);
-  
-  array_maker(count, splited_cnt, 2, 0);
-  input_inteder_ary(splited_cnt, count_ary, DIGITS_NUM);
-
-  put_line();
-  print_array(temperature_ary);
-  put_line();
-  print_array(count_ary);
-  put_line();
->>>>>>> tana/add_arg
 }
 
 int* exchange_NUMtoARY(int num){
@@ -88,11 +42,7 @@ int* exchange_NUMtoARY(int num){
 
 double powpow(int a, int b){
   double dest = 1;
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> tana/add_arg
   if (b > 0)
     for (int i = 0; i < b; i++) dest *= (double)a;
   if (b < 0)
@@ -101,45 +51,26 @@ double powpow(int a, int b){
   return dest;
 }
 
-<<<<<<< HEAD
 int* split_Numerical_Pos(double tmp){
   int ary[TMP_WIDTH];
   int* dest = ary;
   int i, j;
 
-=======
-int* split_Numerical_Pos(double tmp, int beginning, int end){
-  const int width = beginning - end + 1;
-  int ary[width];
-  int* dest_num = ary;
-  int  i, j, k = 0;
-  
->>>>>>> tana/add_arg
   tmp += 0.05; 
-    for (i = beginning; i > (end-1); i--){ 
+  
+  for (i = 1; i > -2; i--){ 
     for (j = 0; tmp >= powpow(10, i); j++) tmp -=powpow(10, i);
-    ary[k] = j;
-    k++;
+    ary[1-i] = j;
   }
   return dest;
 }
 
-<<<<<<< HEAD
 void array_maker(double tmp, int* toInput){
   for (int i = 0; i < TMP_WIDTH; i++) toInput[i] = split_Numerical_Pos(tmp)[i];
 }
 
 void input_inteder_ary(int* splited_num, int segment_array[TMP_WIDTH][SEGMENT_NUM]){
   for (int i = 0; i < TMP_WIDTH; i++)
-=======
-void array_maker(double tmp, int* toInput, int beginning, int end){
-  const int width = beginning - end;
-  for (int i = 0; i < width; i++) toInput[i] = split_Numerical_Pos(tmp, beginning, end)[i];
-}
-
-void input_inteder_ary(int* splited_num, int segment_array[DIGITS_NUM][SEGMENT_NUM], int digits){
-  for (int i = 0; i < digits; i++)
->>>>>>> tana/add_arg
     for (int j = 0; j < SEGMENT_NUM; j++)
       segment_array[i][j] = exchange_NUMtoARY(splited_num[i])[j];
 }

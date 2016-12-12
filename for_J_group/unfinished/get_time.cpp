cc40dc4aa59b3a0b0b49a0818331c36fb333bef7
tanacchi <q111026d@mail.kyutech.jp>
2016-12-12 11:49:59 +0900

    Add output data in get_time.cpp

--- for_J_group/unfinished/get_time.cpp
+++ for_J_group/unfinished/get_time.cpp
@@ -1,15 +1,21 @@
 #include <time.h>
-#include <stdio.h>
+#include <iostream>
 
 int main(void){
   
   time_t timer;
   struct tm *t_st;
+  int hour, min;
+  int for_sevseg;
   
   t_st = localtime(&timer);
+  hour = t_st->tm_hour;
+  min = t_st->tm_min;
+  for_sevseg = hour * 100 + min;
   
-  printf("Month:%d\n", t_st->tm_hour);
-  printf("Day:%d\n", t_st->tm_min);
-  
+  std::cout << "Hour:" << hour << '\n';
+  std::cout << "Minute:" << min << '\n';
+  std::cout << "Output:" << for_sevseg << '\n';
+   
   return 0;
 }

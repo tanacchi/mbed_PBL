c55aae7eec3d9ccd8db6d63c79f5b1658be0520e
tanacchi <q111026d@mail.kyutech.jp>
2016-12-16 13:33:02 +0900

    Finish starter_switch Draft

--- provisional_ver/starter_switch.cpp
+++ provisional_ver/starter_switch.cpp
@@ -1,12 +1,32 @@
+#define SWITCH_A p??
+#define SWITCH_B p??
+
+DigitalIn tact_switch[2] = {
+  DigitalIn (SWITCH_A),
+  DigitalIn (SWITCH_B)
+};
+
 int starter_switch(){
-  int mode;
-  DigitalIn switch[] = {
-
-
-  };
-  for (int i = 0; i < 10^6; i++{
-
-  }
+  int mode = 0;
+  
+  for (int i = 0; i < 10^5; i++)
+    if (mode = mode_reader() != 0) return mode;
   
   return mode;
+}
+
+int switch_reader(int ch){
+  if (tact_switch[ch] == 1) return 1;
+  else return 0;
+}
+
+int mode_reader(){
+  if ((switch_reader[0] == 0) || (switch_reader[1] == 0))
+    return 0;
+  if ((switch_reader[0] == 1) || (switch_reader[1] == 0))
+    return 1;
+  if ((switch_reader[0] == 0) || (switch_reader[1] == 1))
+    return 2;
+  if ((switch_reader[0] == 1) || (switch_reader[1] == 1))
+    return 3;
 }

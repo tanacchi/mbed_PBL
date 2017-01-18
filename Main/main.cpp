218e7045bd1f3ea64d7ebab15f5eb5398c6e41d5
tanacchi <q111026d@mail.kyutech.jp>
2017-01-18 15:23:46 +0900

    Do refactoring 5th edition

--- Main/main.cpp
+++ Main/main.cpp
@@ -12,7 +12,7 @@
 #define DIGITS_NUM 3
 #define NUM_PATTERN 10
 #define MBED_LED_NUM 4
-#define WAIT_PATTERN 5
+#define WAIT_NUM 5
 
 #define SEGMENT_A p10
 #define SEGMENT_B p11
@@ -75,29 +75,27 @@ public:
 
 // ------------------------- Function prototype --------------------------------
 
-double powpow(int a, int b);
-double get_Temperature();
-void digits_init();
-int* convert_NUMintoARY(int element);
-void output_digit(int out_digit[SEGMENT_NUM]);
-void Thermometer();
-void Counter();
-int minute_counter();
-double average_Temperature();
-double tmp_stopper();
-int mode_reader();
 int switch_reader(int ch);
-int starter_switch();
-void Err_message();
+int mode_reader();
 void wait_switch_left();
+int starter_switch();
 int mode_switcher();
+int split_count(int count, int maximam);
+void Thermometer();
+double get_Temperature();
+double tmp_stopper();
+void Counter();
 int count_stopper(int current_push);
 int change_param(int counter);
+double powpow(int a, int b);
+int* convert_NUMintoARY(int element);
+void digits_init();
 void mbedLED_init();
-int split_count(int count, int maximam);
-void disp_limit_LED(int lim);
-void disp_limit_sevseg(int lim);
+void output_digit(int out_digit[SEGMENT_NUM]);
 void output_array(int inteder_array[DIGITS_NUM][SEGMENT_NUM]);
+void disp_limit_sevseg(int lim);
+void disp_limit_LED(int lim);
+void Err_message();
 
 // ------------------------- Main function -------------------------------------
 
@@ -160,12 +158,12 @@ int mode_switcher() {
     count = 0;
     mbedLED_init();
   }
-  disp_limit_LED(split_count(count, 500));
+  disp_limit_LED(split_count(count, 750));
   return (count++ < 500) ? 1 : 0;
 }
 
 int split_count(int count, int maximam) {
-  int unit = maximam / 5;
+  int unit = maximam / WAIT_NUM;
   int i;
   for (i = 1; (count - unit * i) > 0; i++) ;
   return i - 1;
@@ -317,7 +315,7 @@ void output_array(int inteder_array[DIGITS_NUM][SEGMENT_NUM]) {
 // -------------------------- Some extra code ----------------------------------
 
 void disp_limit_sevseg(int lim) {
-  int wait_array[WAIT_PATTERN][SEGMENT_NUM] = {
+  int wait_array[WAIT_NUM][SEGMENT_NUM] = {
     {OFF, OFF, OFF, ON,  OFF, OFF, OFF},
     {OFF, OFF, ON,  OFF, ON,  OFF, OFF},
     {OFF, OFF, OFF, OFF, OFF, OFF, ON },
